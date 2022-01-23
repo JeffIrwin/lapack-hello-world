b9a685389a039fadb35c24e236fad83d5de89362
Jeff Irwin <jirwin505@gmail.com>
2022-01-23 16:08:32 -0500

    Upload LAPACK artifacts

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 
 # lapack-hello-world
 
-Build a simple hello-world program compiling LAPACK (and BLAS) from source with gfortran
+Build a simple hello-world program compiling LAPACK (and BLAS) from source with
+gfortran, on Windows, Linux, and macOS
 
 ## TODO
 
 - fork LAPACK
-- upload/download artifact from previous build, if possible, to avoid
-  recompiling LAPACK on every run
+- download artifact from previous build, if possible, to avoid recompiling
+  LAPACK on every run
 
--- main.f90
+++ main.f90
@@ -6,29 +6,30 @@
 
 program main
 
-	external dgesv
-	integer n, lda, nrhs, ldb
-	parameter (n=2,lda=2,nrhs=1,ldb=2)
-	double precision A(lda,n)
-	double precision b(ldb,nrhs)
-	integer ipiv(n), info, i, j
+	integer, parameter :: n=2, lda=2, nrhs=1, ldb=2
 
-	A(1,1) = 1
-	A(1,2) = 2
-	A(2,1) = 3
-	A(2,2) = 4
+	double precision :: a(lda,n), b(ldb,nrhs)
 
-	B(1,1) = 5
-	B(2,1) = 6
+	integer :: ipiv(n), info, i, j
+
+	a(1,1) = 1
+	a(1,2) = 2
+	a(2,1) = 3
+	a(2,2) = 4
+
+	b(1,1) = 5
+	b(2,1) = 6
 
 	print *, "Hello world"
 
-	call dgesv(n, nrhs, A, lda, ipiv, b, ldb, info)
+	! Solve linear system a*x = b, for x
+	call dgesv(n, nrhs, a, lda, ipiv, b, ldb, info)
 
 	print *, "info = ", info
+	print *, "a (factored) = ", a
 
-	print *, "A (factored) = ", A
-	print *, "x = ", B
+	! Expected output: x = -4.0  4.5
+	print *, "x = ", b
 
 	print *, "done main"
 
